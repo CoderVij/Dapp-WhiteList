0ab8deca1dd05e20d6b992d2fef9e5f8c66e7cc1
CoderVij <contactfreakout@gmail.com>
2022-09-22 17:17:22 +0530

    removed redundant import lib

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
-import Image from 'next/image'
+
 import styles from '../styles/Home.module.css'
 import Web3Modal from "web3modal";
 import {providers, Contract} from "ethers";
 import {useEffect, useRef, useState} from "react";
 import {WHITELIST_CONTRACT_ADDRESS, abi} from "../constants";
-import { network } from 'hardhat';
+
 
 export default function Home() {
 
